75f3439ea2e4dc6bd17a41deb985a4534f2587ca
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-05 21:09:03 +0800

    app/page.tsx: Add likes - add new like - delete like - authenticated users only

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,15 @@ export default async function Home() {
     <>
       <AuthButtonServer/>
       <NewTweet />
-      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(tweets, null, 2)}</pre> */
+      tweets?.map((tweet) => (
+        <div key = {tweet.id}>
+          <p>{tweet.profiles?.name} {tweet.profiles?.username}</p>
+          <p>{tweet.title}</p>
+        </div>
+      ))}
+
+
     </>
   );
 }
