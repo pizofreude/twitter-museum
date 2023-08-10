1ea1a5fce6e07e1de584aa94401ede2d9412ffb1
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-10 23:40:01 +0800

    Add responsive design & styling: Files affected - app/new-tweets.tsx - app/page.tsx - app/tweet.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
     redirect('/login');
   }
 
-  const { data } = await supabase.from('tweets').select('*, author: profiles(*), likes(user_id)');
+  const { data } = await supabase.from('tweets').select('*, author: profiles(*), likes(user_id)').order('created_at', { ascending: false});
   const tweets = data?.map(tweet => ({
     ...tweet,
     // author: tweet.author as Profile
@@ -26,12 +26,19 @@ export default async function Home() {
   })) ?? [];
 
   return (
-    <>
-      <AuthButtonServer/>
-      <NewTweet />
+    <div className="w-full max-w-xl
+    mx-auto">
+      <div className="flex justify-between
+      px-4 py-6 border border-gray-800
+      border-t-0">
+        <h1 className="text-xl
+        font-bold">Home</h1>
+        <AuthButtonServer/>
+      </div>
+      <NewTweet user={session.user}/>
       <Tweets tweets={tweets} />
 
 
-    </>
+    </div>
   );
 }
--- app/tweet.tsx
+++ app/tweet.tsx
@@ -4,6 +4,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Likes from "./likes";
 import { useEffect, experimental_useOptimistic as useOptimistic } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Tweet({ tweets }: { tweets: TweetWithAuthor[] }) {
     const [optimisticTweets, addOptimisticTweet] = useOptimistic<TweetWithAuthor[], TweetWithAuthor> (
@@ -38,10 +39,24 @@ export default function Tweet({ tweets }: { tweets: TweetWithAuthor[] }) {
     }, [supabase, router]);
 
     return optimisticTweets.map((tweet) => (     
-        <div key = {tweet.id}>
-          <p>{tweet.author.name} {tweet.author.username}</p>
-          <p>{tweet.title}</p>
-          <Likes tweet={tweet} addOptimisticTweet={addOptimisticTweet}/>
+        <div key = {tweet.id} className="border border-gray-800 border-t-0 px-4 py-8 flex">
+          <div className="h-12 w-12">
+            <Image
+              className="rounded-full"
+              src={tweet.author.avatar_url}
+              alt="user avatar"
+              width={48}
+              height={48}
+            />
+          </div>
+          <div className="ml-4">
+            <p>
+              <span className="font-bold">{tweet.author.name}</span>
+              <span className="text-sm ml-2 text-gray-400">{tweet.author.username}</span>
+            </p>
+            <p>{tweet.title}</p>
+            <Likes tweet={tweet} addOptimisticTweet={addOptimisticTweet}/>
+          </div>
         </div>
       ));
 }
