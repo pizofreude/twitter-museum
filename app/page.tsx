b80039cc59f0a18b0fdae760fb8cba674a933619
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-13 00:08:41 +0800

    app/page.tsx: Fix tweets type bug with !!

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-
 import { cookies } from "next/headers";
 import AuthButtonServer from './auth-button-server';
 import { redirect } from 'next/navigation';
 import NewTweet from './new-tweets';
-import Likes from './likes';
 import Tweets from './tweet';
 
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const supabase = createServerComponentClient <Database>({ cookies });
@@ -21,24 +20,18 @@ export default async function Home() {
     ...tweet,
     // author: tweet.author as Profile
     author: Array.isArray(tweet.author) ? tweet.author[0] : tweet.author,
-    user_has_liked_tweet: tweet.likes.find(like => like.user_id === session.user.id),
+    user_has_liked_tweet: !!tweet.likes.find(like => like.user_id === session.user.id),
     likes: tweet.likes.length
   })) ?? [];
 
   return (
-    <div className="w-full max-w-xl
-    mx-auto">
-      <div className="flex justify-between
-      px-4 py-6 border border-gray-800
-      border-t-0">
-        <h1 className="text-xl
-        font-bold">Home</h1>
+    <div className="w-full max-w-xl mx-auto">
+      <div className="flex justify-between px-4 py-6 border border-gray-800 border-t-0">
+        <h1 className="text-xl font-bold">Home</h1>
         <AuthButtonServer/>
       </div>
-      <NewTweet user={session.user}/>
+      <NewTweet user={session.user} />
       <Tweets tweets={tweets} />
-
-
     </div>
   );
 }
