f47e68c59dc784f568aaef863232f031b0948c6d
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-12 22:44:32 +0800

    app/login/page.tsx: Render login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import AuthButtonClient from "../auth-button-client";
+import GitHubButton from "./login-button";
 
 export default async function Login() {
     const supabase = createServerComponentClient<Database>({ cookies });
@@ -14,5 +15,9 @@ export default async function Login() {
         redirect("/");
     }
 
-    return <AuthButtonClient session={session}/>;
+    return (
+        <div className="flex-1 flex justify-center items-center">
+            <GitHubButton/>
+        </div>
+    ) 
 }
